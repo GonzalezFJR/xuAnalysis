68800bd999db5d5023446f278253dbe1edc6325f
Juan Rodrigo Gonzalez Fernandez <juan.rodrigo.gonzalez.fernandez@cern.ch>
2019-07-02 17:12:24 +0200

    Adding GetLumi func

--- framework/functions.py
+++ framework/functions.py
@@ -274,3 +274,36 @@ def GetLbMass(lep, jets, btags):
   elif len(btags) >= 2:
     m = (lep.p + GetObjMinDR(lep, btags).p).M()
   return m
+
+def GetLumi(year, era = 'all'):
+  lumi = {
+    2016 : {
+             'B' : 5.783,
+             'C' : 2.5639,
+             'D' : 4.248,
+             'E' : 4.0089,
+             'F' : 3.101,
+             'G' : 3.1013,
+             'H' : 7.54,
+             'all': 35.92,
+           },
+    2017 : {
+             'B' : 4.823,
+             'C' : 9.664,
+             'D' : 4.252,
+             'E' : 9.278,
+             'F' : 13.540,
+            'all': 41.53,
+             'G' : 0.297,
+             'H' : 0.22,
+           },
+     2018 : {
+              'A' :  14.00,
+              'B' :   7.10,
+              'C' :   6.94,
+              'D' :  31.93,
+              'all': 59.74,
+            }
+  }
+  return lumi[year][era]
+

d078c87c42bafd4e6148cfaa2d4df1066993a963
GonzalezFJR <juan.rodrigo.gonzalez.fernandez@cern.ch>
2019-07-17 12:36:23 +0200

    Writing  ProcessDic.py

--- plotter/ProcessDic.py
+++ plotter/ProcessDic.py
@@ -1,6 +1,10 @@
 '''
  Get a dictionary with histograms and compute all the uncertainties
  # dic['process']['histoname']
+ h = HistoMerger(dic, ListOfProcesses, ListOfUncertainties, NameOfHistogram)
+ h.SumHistos()
+ hstat = h.GetUncHist('stat')
+ hsyst = h.GetUncHist()
 '''
 
 class HistoMerger:
@@ -53,6 +57,67 @@ class HistoMerger:
             if not hs in self.indic[pr].keys(): hs = hname
             h.Add(self.indic[pr][hname])
         self.sumdic[s] = h
+
+  def GetDifUnc(self, hnom, hsyst):
+    ''' Get a nom histo and a list of histos and returns the mean per-bin error in a list 
+        hsyst is a list of labels, not name of uncertainty
+    '''
+    nb = hnom.GetNbinsX()
+    n = [hnom.GetBinContent(i) for i in range(0, nb+2)]
+    if not isinstance(hsyst, list): hsyst = [hsyst]
+    var  = [ [abs(h.GetBinContent(i)-n[i]) for i in range(0, nb+2)] for h in hsyst]
+    nsyst = len(var)
+    meansyst = []
+    for i in range(0, nb+2):
+      l = 0.
+      for j in range(nsyst):
+        l += var[j][i]
+      l = l/nsyst
+      meansyst.append(l)
+    return meansyst
+
+  def GetStatUnc(self):
+    ''' List with stat unc from nominal histo '''
+    hnom = self.sumdic['']
+    nb = hnom.GetNbinsX()
+    statunc = [hnom.GetBinError(i) for i in range(0, nb+2)]
+    return statunc
+
+  def GetSum2Unc(self, lsyst = '', includeStat = False):
+    ''' From a list of uncertainties, get a list of per-bin unc w.r.t. nominal histo 
+        lsyst is a list of uncertainty names, not labels
+    '''
+    if   lsyst == '': lsyst = self.systname
+    elif isinstance(lsyst, str) and ',' in lsyst: lsyst = lsyst.split(',')
+    elif not isinstance(lsyst, list): lsyst = [syst]
+    unc = []
+    systExist = labmda x : x in self.systlabels
+    for s in lsyst:
+      listOfGoodSyst = filter(systExist, self.GetListOfCandNames(s))
+      unc.append(syst, GetDifUnc(self.sumdic[''], [sumdic[s] for s in listOfGoodSyst]))
+    sum2 = []
+    if includeStat: sum2.append(self.GetStatUnc())
+    for iunc in unc:
+      val = 0
+      for v in iunc: val += v*v
+      v = sqrt(v)
+      sum2.append(v)
+    return sum2
+
+  def GetUncHist(self, syst = ''):
+    ''' syst is a name, not a label... returns nominal histo with nice uncertainties '''
+    if   syst == '': syst = self.systname
+    elif isinstance(syst, str) and ',' in syst: syst = syst.split(',')
+    elif not isinstance(syst, list): syst = [syst]
+    sumdic = self.sumdic.copy() # To keep the original
+    hnom = sumdic[''].Clone('hnom')
+    if syst == 'stat': return hnom
+    unc = self.GetSum2Unc(syst)
+    nbins = hnom.GetNbinsX()
+    for i in range(0,nbins+2)
+      hnom.SetBinError(unc[i])
+    hnom.SetDirectory(0)
+    return hnom
       
   def __init__(self, indic, prlist = [], syslist = [], hname = ''):
     self.indic = indic

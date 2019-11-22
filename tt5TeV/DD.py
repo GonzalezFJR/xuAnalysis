cc787b51632560aa0081ca263657fbad951528de
Juan Rodrigo Gonzalez Fernandez <juan.rodrigo.gonzalez.fernandez@cern.ch>
2019-11-22 14:58:56 +0100

    Updates on tt5TeV analysis

--- tt5TeV/DD.py
+++ tt5TeV/DD.py
@@ -13,7 +13,9 @@ datasamples = processDic['data']
 
 def DrawDYDD(lev = '2jets', doSF = False):
   if not doSF and lev == 'MET': return 
-  d = DYDD(path,outpath,'ElMu',lev, DYsamples=DYsamples, DataSamples=datasamples, lumi=Lumi, histonameprefix='', hname = 'DYHistoElMu')
+  d = DYDD(path,outpath,'ElMu',lev, DYsamples=DYsamples, DataSamples=datasamples, lumi=Lumi, histonameprefix='')  #, hname = 'DYHistoElMu' if not doSF else 'DYHisto')
+  d.SetHistoNameOF('DYHistoElMu')
+  d.SetHistoNameSF('DYHisto')
   lab = 'SF' if doSF else 'OF'
   d.PrintDYestimate(doSF,  'DYDD_'+lev+'_'+lab)
 
@@ -21,17 +23,18 @@ def DrawDYDDnjets(lev='2jets'):
   d = DYDD(path,outpath,lev)
   d.PrintDYSFnjets()
 
-def DrawNonprompt(lev = '2jets'):
-  d = NonpromptDD(path,outpath,'ElMu',lev, process=processDic , lumi=Lumi, histonameprefix='',yieldsSSname='YieldsSS')
+def DrawNonprompt(lev = '2jets', ch = 'ElMu'):
+  d = NonpromptDD(path,outpath,ch,lev, process=processDic , lumi=Lumi, histonameprefix='',yieldsSSname='YieldsSS')
   d.PrintSSyields('SSyields_'+lev, lev)
-  for chan in ['ElMu']: #['ElEl', 'MuMu', 'ElMu']:
+  for chan in ['ElEl', 'MuMu', 'ElMu']:
     d.PrintNonpromptEstimate('NonpromptDD_'+lev+'_'+chan, chan, lev)
 
 # DY
-for ilev in ['dilepton','2jets']:
-  DrawDYDD(lev=ilev)
+#for ilev in ['dilepton','2jets']:
+DrawDYDD('2jets')
+DrawDYDD('2jets',True)
 
 # Nonprompt 
-for ilev in ['2jets']: #['dilepton','MET','1btag','2jets', 'ZVeto']:
-  DrawNonprompt(lev=ilev)
+#for ilev in ['2jets']: #['dilepton','MET','1btag','2jets', 'ZVeto']:
+#  DrawNonprompt(ilev)
 
--- tt5TeV/xsec.py
+++ tt5TeV/xsec.py
@@ -12,15 +12,16 @@ datasamples = processDic['data']
 def xsec(chan = 'ElMu', lev = '2jets', doDD = False):
   x = CrossSection(outpath, chan, lev)
   x.SetTextFormat("tex")
+  x.SetPathToTrees('/pool/ciencias/userstorage/juanr/nanoAODv4/5TeV/5TeV_5sept/')
   bkg = []
   bkg.append(['tW',            process['tW'],   0.30])
-  if not doDD:
+  bkg.append(['Nonprompt lep', process['Nonprompt'], 0.50])
+  if not doDD or chan == 'MuMu' or chan == 'ElEl':
     bkg.append(['DY',            process['DY'],   0.15])
-    bkg.append(['Nonprompt lep', process['Nonprompt'], 0.50])
   bkg.append(['VV',            process['VV'],   0.30])
   signal   = ['tt',            process['tt']]
   data     = process['data']
-  expunc = "MuonEff, ElecEff, JES, JER, TrigEff, Prefire" # JER
+  expunc = "MuonEff, ElecEff, TrigEff, Prefire, JES"#, JES, JER, TrigEff, Prefire" # JER
   modunc = "pdf, scale, isr, fsr"
 
   x.ReadHistos(path, chan, lev, bkg = bkg, signal = signal, data = data, expUnc = expunc, modUnc = modunc)
@@ -30,13 +31,26 @@ def xsec(chan = 'ElMu', lev = '2jets', doDD = False):
   x.AddModUnc('hdamp','TT_hdampUP','TT_hdampDOWN')
 
   if doDD:
-    d = DYDD(path,outpath,chan,lev, DYsamples=DYsamples, DataSamples=datasamples, lumi=Lumi, histonameprefix='', hname = 'DYHistoElMu')
+    d = DYDD(path,outpath,chan,lev, DYsamples=DYsamples, DataSamples=datasamples, lumi=Lumi, histonameprefix='', hname = 'DYHistoElMu' if chan == 'ElMu' else 'DYHisto')
     DYy, DYerr = d.GetDYDD()
     x.AddBkg('DYDD', DYy, 0, 0.15, DYerr)
+    '''
+    d = DYDD(path,outpath,chan,lev, DYsamples=DYsamples, DataSamples=datasamples, lumi=Lumi, histonameprefix='', hname = 'DYHistoElMu')
+    if chan == 'ElMu': 
+      DYy, DYerr = d.GetDYDD()
+      x.AddBkg('DYDD', DYy, 0, 0.15, DYerr)
+    else:
+      SF, SFerr = d.GetScaleFactor()
+      dy = x.GetBkg('DY').GetYield()
+      print 'dy = ', dy
+      print 'dy = ', dy*SFerr
+      x.GetBkg('DY').SetYield(dy*SF)
+      x.GetBkg('DY').SetStatUnc(dy*SFerr)
+    '''
 
-    f = NonpromptDD(path, chan=chan, level=lev, process=processDic , lumi=Lumi, histonameprefix='',yieldsSSname='YieldsSS')
-    fy, fe = f.GetNonpromptDD(chan)
-    x.AddBkg('Nonprompt lep', fy, 0, 0.30, fe)
+    #f = NonpromptDD(path, chan=chan, level=lev, process=processDic , lumi=Lumi, histonameprefix='',yieldsSSname='YieldsSS')
+    #fy, fe = f.GetNonpromptDD(chan)
+    #x.AddBkg('Nonprompt lep', fy, 0, 0.30, fe)
 
   suf = '_'+chan+'_'+lev+'_'+('DD' if doDD else 'MC')
   x.PrintYields('Yields'+suf)
@@ -45,4 +59,5 @@ def xsec(chan = 'ElMu', lev = '2jets', doDD = False):
 
 lev = '2jets'
 xsec('ElMu',lev,1)
-#xsec('MuMu',lev)
+#xsec('MuMu',lev,1)
+#xsec('ElEl',lev,1)

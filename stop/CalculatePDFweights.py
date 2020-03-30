9447e8f2533ef742ca7fa50445175a533b9c7bea
Juan Rodrigo Gonzalez Fernandez <juan.rodrigo.gonzalez.fernandez@cern.ch>
2020-03-30 12:47:41 +0200

    Adding scripts to calculate uncertainties

---
+++ stop/CalculatePDFweights.py
@@ -0,0 +1,222 @@
+'''
+ python CreateRootFiles.py -s tt -n 100 --sendJobs --mStop 275 --mLSP 100 #-q short
+'''
+
+import os, sys
+from config import *
+basepath = os.path.abspath(__file__).rsplit('/xuAnalysis/',1)[0]+'/xuAnalysis/'
+sys.path.append(basepath)
+from plotter.TopHistoReader import TopHistoReader, HistoManager
+from plotter.Plotter import Stack, HistoComp
+from framework.looper import looper
+from ROOT.TMath import Sqrt as sqrt
+from ROOT import kRed, kOrange, kBlue, kTeal, kGreen, kGray, kAzure, kPink, kCyan, kBlack, kSpring, kViolet, kYellow
+from ROOT import TCanvas, gROOT, TLegend, TCanvas
+from numpy import arange
+gROOT.SetBatch(1)
+import argparse
+
+parser = argparse.ArgumentParser(description='To select masses and year')
+parser.add_argument('--year',         default=2018   , help = 'Year')
+parser.add_argument('--mStop',        default=275  , help = 'Stop mass')
+parser.add_argument('--mLSP',         default=100  , help = 'Neutralino mass')
+parser.add_argument('--BS',          action='store_true'  , help = 'Do BS region')
+parser.add_argument('--SR',          action='store_true'  , help = 'Do signal region')
+parser.add_argument('--region',       default='SR'  , help = 'Select the region')
+parser.add_argument('--sendJobs','-j'   , action='store_true'  , help = 'Send jobs!')
+ 
+#args = parser.parse_args()
+args, unknown = parser.parse_known_args()
+
+ms   = int(args.mStop)
+ml   = int(args.mLSP)
+year = int(args.year)
+sendJobs = args.sendJobs
+argprocess = ''
+
+treeName="MiniTree"
+nSlots = 1
+
+# Set signal masses
+region = args.region
+if args.BS: region = 'BS'
+if args.SR: region = 'SR'
+
+# Set constants
+outpath = '/nfs/fanae/user/juanr/CMSSW_10_2_5/src/xuAnalysis/stop_v629Mar/Unc/%s_PDFunc/%i/mass%i_%i/'%(region, year, ms, ml)
+os.system("mkdir -p %s"%outpath)
+model = '/nfs/fanae/user/juanr/CMSSW_10_2_5/src/xuAnalysis/TopPlots/DrawMiniTrees/NNtotal_model2.h5'
+# Create the looper, set readOutput to true to read previous temporary rootfiles created for each sample
+pathToFiles = '/nfs/fanae/user/juanr/test/PAFnanoAOD/PDFminitree/'#path[year]
+l = looper(path=pathToFiles if region=='SR' else pathBS[year], nSlots=nSlots, treeName = 'MiniTree', options = 'merge', outpath = outpath+'tempfiles/', readOutput=True)#, sendJobs=sendJobs)
+
+# Add processes
+processes = ['tt']
+if argprocess != '': processes = argprocess
+l.AddSample('tt', processDic[year]['tt'])
+
+# Systematic uncertainties
+#syst = 'MuonEff, ElecEff, Trig, JESCor, JESUnCor, JER, MuonES, ElecES, Uncl, Btag, MisTag, PU, TopPt, FSR, ISR, UE, hdamp, mtop' # PDF, ME
+#syst += ',ME0,ME1,ME2,ME3,ME4,ME5,ME6,ME7,ME8'
+
+systlist = []#[x+y for x in syst.replace(' ','').split(',') for y in ['Up','Down']]
+
+# Lines below to read the DNN values
+l.AddHeader('from framework.mva import ModelPredict\n')
+l.AddInit('      self.pd1 = ModelPredict("%s")\n'%model)
+l.AddInit("      self.systematics = ['']\n")
+l.AddSyst(systlist)
+loopcode = '''
+values = [%i,  %i, t.TDilep_Pt, t.TDeltaPhi, t.TDeltaEta, t.TLep0Pt, t.TLep0Eta, t.TLep1Pt, vmet, t.TLep1Eta, vmll, vmt2, vht]
+prob1 = self.pd1.GetProb(values)
+'''%(ms, ml)
+l.AddLoopCode(loopcode)
+
+selection = '''
+ passNonprompt = (t.TStatus == 1 or  t.TStatus == 22)
+ if not passNonprompt: return 
+'''
+l.AddSelection(selection)
+
+# Define expresions, including varibles with syst variations
+l.AddExpr('deltaphi', 'TDeltaPhi', 'abs(TDeltaPhi)/3.141592')
+l.AddExpr('deltaeta', 'TDeltaEta', 'abs(TDeltaEta)')
+l.AddExpr('exprW', ['TWeight'], 'TWeight')
+l.AddExpr('vmet', ['TMET'], 'TMET')
+l.AddExpr('vmt2', ['TMT2'], 'TMT2')
+l.AddExpr('vht',  ['THT' ], 'THT')
+l.AddExpr('vmll', ['TMll'], 'TMll')
+l.AddExpr('vpd', '', 'prob1', True)
+
+# Weight and cuts...
+l.AddCut('t.TIsSS == 0', [])
+l.AddCut('TPassDilep == 1', 'TPassDilep')
+if region == 'SR':
+  l.AddCut('TMET >= 50', 'TMET')
+  l.AddCut('TMT2 >= 80', 'TMT2')
+elif region == 'ttMET' or region == 'ttmet':
+  l.AddCut('TMT2 < 80', 'TMT2')
+elif region == 'ttMT2' or region == 'ttmt2':
+  l.AddCut('TMET < 50', 'TMET')
+elif region == 'CR':
+  l.AddCut('(TMET < 50 or TMT2 < 80)', ['TMET', 'TMT2'])
+l.AddCut('TNJets >= 2', 'TNJets')
+l.AddCut('TNBtags >= 1', 'TNBtags')
+
+# Add histograms
+cut = ''
+nPDFweights = 33 if year != 2016 else 100; nMEweights = 9;
+tags = ['PDF']*nPDFweights + ['ME']*nMEweights
+for tag,i in zip(tags, range(nPDFweights)+range(nMEweights)):
+  l.AddHisto('vpd',  'dnn_'+'%s%i'%(tag,i),  20, 0, 1,   weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('vpd',  'dnn_10bins_'+'%s%i'%(tag,i),  10, 0, 1,   weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('vpd',  'dnn_30bins_'+'%s%i'%(tag,i),  30, 0, 1,   weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('vpd',  'dnn_40bins_'+'%s%i'%(tag,i),  40, 0, 1,   weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('vpd',  'dnn_5bins_'+'%s%i'%(tag,i),  5, 0, 1,   weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('TMll', 'mll_'+'%s%i'%(tag,i), 30, 0, 300, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  if region == 'SR':
+    l.AddHisto('TMET', 'met_'+'%s%i'%(tag,i), 25, 50, 300, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+    l.AddHisto('TMET', 'met_5bins_'+'%s%i'%(tag,i), 5, 50, 300, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+    l.AddHisto('TMT2', 'mt2_'+'%s%i'%(tag,i), 8, 80, 160, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  else:
+    l.AddHisto('TMET', 'met_'+'%s%i'%(tag,i), 30, 0, 300, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+    l.AddHisto('TMT2', 'mt2_'+'%s%i'%(tag,i), 16, 0, 160, weight = 'TWeight_%s%i'%(tag,i), cut = 'vmt2 > 0')
+  l.AddHisto('TDilep_Pt', 'dileppt_'+'%s%i'%(tag,i), 30, 0, 300, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('TLep0Pt', 'lep0pt_'+'%s%i'%(tag,i), 20, 0, 200, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('TLep1Pt', 'lep1pt_'+'%s%i'%(tag,i), 15, 0, 150, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('TLep0Eta', 'lep0eta_'+'%s%i'%(tag,i), 30, -2.4, 2.4, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('TLep1Eta', 'lep1eta_'+'%s%i'%(tag,i), 30, -2.4, 2.4, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('deltaphi', 'deltaphi_'+'%s%i'%(tag,i), 30, 0, 1, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('deltaeta', 'deltaeta_'+'%s%i'%(tag,i), 30, 0, 5, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('TJet0Pt', 'jet0pt_'+'%s%i'%(tag,i), 30, 0, 450, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('TJet1Pt', 'jet1pt_'+'%s%i'%(tag,i), 30, 0, 300, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('TJet0Eta', 'jet0eta_'+'%s%i'%(tag,i), 30, -2.4, 2.4, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('TJet1Eta', 'jet1eta_'+'%s%i'%(tag,i), 30, -2.4, 2.4, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('THT', 'ht_'+'%s%i'%(tag,i), 40, 0, 800, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('TNJets', 'njets_'+'%s%i'%(tag,i), 6, 1.5, 7.5, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  l.AddHisto('TNBtags', 'nbtags_'+'%s%i'%(tag,i), 3, 0.5, 3.5, weight = 'TWeight_%s%i'%(tag,i), cut = '')
+  
+histos  = ['mt2',  'met',  'mll',  'dnn',  'dileppt',  'deltaphi',  'deltaeta',  'ht',  'lep0pt',  'lep1pt',  'lep0eta',  'lep1eta', 'njets', 'nbtags', 'jet0pt', 'jet1pt', 'jet0eta', 'jet1eta'] 
+histos += ['dnn_5bins', 'dnn_10bins', 'dnn_30bins', 'dnn_40bins']
+
+out = l.Run()
+
+if sendJobs: exit()
+
+t = TopHistoReader(outpath+'tempfiles/')
+for var in histos:
+  PDFhistosNom = t.GetNamedHisto('met_PDF0', 'tt')
+  PDFhistos = [t.GetNamedHisto(var+'_PDF%i'%i, 'tt') for i in range(1, nPDFweights-2 if year != 2016 else nPDFweights)]
+  AlphaShistos = [t.GetNamedHisto(var+'_PDF%i'%(nPDFweights-2), 'tt'), t.GetNamedHisto('met_PDF%i'%(nPDFweights-1), 'tt')] if year != 2016 else []
+  pdfUp, pdfDown = GetPDFuncHisto(PDFhistosNom, PDFhistos, AlphaShistos, prName = 'tt')
+
+  MEhistos = [t.GetNamedHisto(var+'_ME%i') for i in range(nMEweights)]
+  meUp, meDown = GetMEuncHisto(MEhistos)
+  fout = TFile.Open(outpath+var+'.root')
+  pdfUp.Write(); pdfDown.Write();
+  meUp.Write(); meDown.Write();
+  fout.Close()
+
+from math import sqrt
+def GetPDFuncHisto(nom, pdfvars, alphasvar = [], prName = 'tt'):
+  # Using PDF4LHC15_nlo_nf4_30_pdfas, 1+30+2 weights, see Table 6 in 
+  # https://arxiv.org/pdf/1510.03865.pdf
+  # Eq [20] for PDF unc:  0.01 (0.45 %)
+  # Eq [27] for alpha_S:  0.00 (0.15 %)
+  nbins = nom.GetNbinsX()+1
+  up = nom.Clone(prName+'_PDFUp')
+  do = noo.Clone(prName+'_PDFDown')
+  unc      = [0]*nbins
+  alphaunc = [0]*nbins
+  for var in pdfvars:
+    for i in range(0, nbins):
+      n = nom.GetBinContent(i)
+      v = var.GetBinContent(i)
+      unc[i] += (n-v)*(n-v)
+  unc = [sqrt(x) for x in unc]
+
+  if len(alphasvar) == 2:
+    for i in range(0, nbins):
+      vu = alphasvar[0].GetBinContent(i)
+      vd = alphasvar[1].GetBinContent(i)
+      alphaunc[i] = (vu-vd)/2
+    unc = [sqrt(x*x+y*y) for x,y in zip(unc, alphaunc)]
+  
+  for i in range(0, nbins):
+    n = nom.GetBinContent(i)
+    up.SetBinContent(i, n+unc[i])
+    do.SetBinContent(i, n-unc[i])
+  return [up, do]
+
+def GetMEuncHisto(orderedHistos):
+  '''
+  [1] muF = 0.50, muR = 0.50 
+  [2] muF = 0.50, muR = 1.00 
+  [3] muF = 0.50, muR = 2.00 (unphysical)
+  [4] muF = 1.00, muR = 0.50 
+  [5] muF = 1.00, muR = 1.00 (nominal)
+  [6] muF = 1.00, muR = 2.00 
+  [7] muF = 2.00, muR = 0.50 (unphysical)
+  [8] muF = 2.00, muR = 1.00 
+  [9] muF = 2.00, muR = 2.00 
+  '''
+  nom = orderedHistos[4]
+  nbins = nom.GetNbinsX()+1
+  up = nom.Clone(prName+'_ScaleUp')
+  do = noo.Clone(prName+'_ScaleDown')
+  uncUp  = [0]*nbins
+  uncDo  = [0]*nbins
+  hvar = [orderedHistos[0], orderedHistos[1], orderedHistos[3], orderedHistos[5], orderedHistos[7], orderedHistos[8]]
+  
+  for i in range(0, nbins):
+    n = nom.GetBinContent(i)
+    vals  = [h.GetBinContent(i) for h in hvar]
+    diffs = [x-v for v in vals]
+    uncDo[i] = min(diffs+[0])
+    uncUp[i] = max(diffs+[0])
+
+  for i in range(0, nbins):
+    n = nom.GetBinContent(i)
+    up.SetBinContent(i, n+uncUp[i])
+    do.SetBinContent(i, n+uncDo[i])
+  return [up, do]
---
+++ stop/ttmt2Tails.py
@@ -0,0 +1,233 @@
+import os, sys
+#from config import *
+from config import GetLumi, path, pathBS
+basepath = os.path.abspath(__file__).rsplit('/xuAnalysis/',1)[0]+'/xuAnalysis/'
+sys.path.append(basepath)
+from plotter.TopHistoReader import TopHistoReader, HistoManager
+from plotter.Plotter import Stack, HistoComp
+from framework.looper import looper
+from ROOT.TMath import Sqrt as sqrt
+from ROOT import kRed, kOrange, kBlue, kTeal, kGreen, kGray, kAzure, kPink, kCyan, kBlack, kSpring, kViolet, kYellow
+from ROOT import TCanvas, gROOT, TLegend, TCanvas
+gROOT.SetBatch(1)
+import argparse
+
+parser = argparse.ArgumentParser(description='To select masses and year')
+parser.add_argument('--year',         default='2018'   , help = 'Year')
+parser.add_argument('--mStop',        default=275  , help = 'Stop mass')
+parser.add_argument('--mLSP',         default=100  , help = 'Neutralino mass')
+parser.add_argument('--BS',          action='store_true'  , help = 'Do BS region')
+parser.add_argument('--SR',          action='store_true'  , help = 'Do signal region')
+parser.add_argument('--sendJobs','-j'   , action='store_true'  , help = 'Send jobs!')
+ 
+#args = parser.parse_args()
+args, unknown = parser.parse_known_args()
+
+ms   = int(args.mStop)
+ml   = int(args.mLSP)
+year = int(args.year) if args.year.isdigit() else args.year
+sendJobs = args.sendJobs
+
+
+
+region = 'SR'
+if args.BS: region = 'BS'
+if args.SR: region = 'SR'
+treeName="MiniTree"
+syst = 'MuonEff, ElecEff, Trig, JER, MuonES, Uncl, Btag, MisTag, PU, JESCor, JESUnCor, ElecES'
+if year != 2018: syst += ', Pref'
+systlist = [x+y for x in syst.replace(' ','').split(',') for y in ['Up','Down']]
+
+GetOutPath = lambda year, region : '/nfs/fanae/user/juanr/CMSSW_10_2_5/src/xuAnalysis/stop/ttmt2/%s/%s/'%(str(year), region)
+outpath = GetOutPath(year, region) 
+model = '/nfs/fanae/user/juanr/CMSSW_10_2_5/src/xuAnalysis/TopPlots/DrawMiniTrees/NNtotal_model2.h5'
+
+vardic = {
+'mt2'      : 'm_{T2} (GeV)',
+'mt2_4bins': 'm_{T2} (GeV)',
+'met'      : 'MET (GeV)',
+'mll'      : 'm_{e#mu} (GeV)',
+'dnn_5'    : 'DNN score',
+'dnn_10'   : 'DNN score',
+'dnn'      : 'DNN score',
+'count'    : 'Counts',
+'dileppt'  : 'p_{T}^{e#mu} (GeV)',
+'deltaphi' : '#Delta#phi (rad/#pi)',
+'deltaeta' : '#Delta#eta',
+'ht'       : 'H_{T} (GeV)',
+'lep0pt'   : 'Leading lepton p_{T} (GeV)',
+'lep1pt'   : 'Subleading lepton p_{T} (GeV)',
+'lep0eta'  : 'Leading lepton #eta',
+'lep1eta'  : 'Subleading lepton #eta',
+'njets'    : 'Jet multiplicity',
+'nbtags'   : 'b-tag multiplicity',
+}
+#vardic = { 'nbtags'    : 'b-tag multiplicity'}
+
+processDic = {
+2018 : {
+       'stop'       : 'stop',
+       'tt'         : 'TTTo2L2Nu',
+       'ttgaus'     : 'TTTo2L2Nu',
+       'ttnongaus'  : 'TTTo2L2Nu',
+       'tW'  : 'tbarW_noFullHad, tW_noFullHad',
+       'ttZ' : 'TTZToLL_M_1to10, TTZToLLNuNu_M_10_a, TTZToQQ',
+       'Semileptonictt' : 'TTToSemiLeptonic',
+       'ttDilepNonprompt':'TTTo2L2Nu',
+       'tWnonprompt':'tbarW_noFullHad, tW_noFullHad',
+       'Othernonprompt':'WJetsToLNu_MLM, DYJetsToLL_M_10to50_MLM, DYJetsToLL_M_50_a, WWTo2L2Nu, WZTo2L2Q, ZZTo2L2Nu, ZZTo2L2Q',
+       'Others'     : 'TTWJetsToLNu, TTWJetsToQQ, DYJetsToLL_M_10to50_MLM, DYJetsToLL_M_50_a, WWTo2L2Nu, WZTo2L2Q, WZTo3LNu, ZZTo2L2Nu, ZZTo2L2Q, ZZTo4L',
+       'tt_hdampUp' : 'TTTo2L2Nu_hdampUp',
+       'tt_UEUp'  : 'TTTo2L2Nu_TuneCP5Up',
+       'tt_hdampDown' : 'TTTo2L2Nu_hdampDown',
+       'tt_UEDown'  : 'TTTo2L2Nu_TuneCP5Down',
+       'tt_mtopDown'  : 'TTTo2L2Nu_mtop171p5',
+       'tt_mtopUp'  : 'TTTo2L2Nu_mtop173p5',
+       'data' : 'EGamma_2018, MuonEG_2018, SingleMuon_2018',
+     },
+2017 : {
+       'tt'  : 'TTTo2L2Nu',
+       'ttgaus'     : 'TTTo2L2Nu',
+       'ttnongaus'  : 'TTTo2L2Nu',
+       'stop'      : 'stop',
+       'tW'  : 'tbarW_noFullHad, tW_noFullHad',
+       'ttZ' : 'TTZToLL_M_1to10, TTZToLLNuNu_M_10_a, TTZToQQ',
+       'Semileptonictt' : 'TTToSemiLeptonic',
+       'ttDilepNonprompt':'TTTo2L2Nu',
+       'tWnonprompt':'tbarW_noFullHad, tW_noFullHad',
+       'Othernonprompt':'WJetsToLNu_MLM, DYJetsToLL_M_10to50_MLM, DYJetsToLL_M_50_a, WWTo2L2Nu, WZTo2L2Q, ZZTo2L2Nu, ZZTo2L2Q',
+       'Others'     : 'TTWJetsToLNu, TTWJetsToQQ, DYJetsToLL_M_10to50_MLM, DYJetsToLL_M_50_a, WWTo2L2Nu, WZTo2L2Q, WZTo3LNu, ZZTo2L2Nu, ZZTo2L2Q, ZZTo4L',
+       'tt_hdampUp' : 'TTTo2L2Nu_hdampUp',
+       'tt_UEUp'  : 'TTTo2L2Nu_TuneCP5Up',
+       'tt_hdampDown' : 'TTTo2L2Nu_hdampDown',
+       'tt_UEDown'  : 'TTTo2L2Nu_TuneCP5Down',
+       'tt_mtopDown'  : 'TTTo2L2Nu_mtop171p5',
+       'tt_mtopUp'  : 'TTTo2L2Nu_mtop173p5',
+       'data' : 'MuonEG_2017,SingleElectron_2017,SingleMuon_2017',
+     },
+2016 : {
+    'tt'  : 'TT',
+    'ttgaus'     : 'TT',
+    'ttnongaus'  : 'TT',
+    'stop'      : 'stop',
+    'tW'  : 'tbarW_noFullHad, tW_noFullHad',
+    'ttZ' : 'TTZToLL_M_1to10_MLM, TTZToLLNuNu_M_10_a,TTZToQQ',#, TTZToQQ',
+    'Semileptonictt' : 'TTToSemiLeptonic',
+    'ttDilepNonprompt':'TT',
+    'tWnonprompt':'tbarW_noFullHad, tW_noFullHad',
+    'Othernonprompt':'DYJetsToLL_M_10to50, DYJetsToLL_M_50_a, WWTo2L2Nu, WZTo2L2Q, ZZTo2L2Nu, ZZTo2L2Q',
+    'Others'     : 'TTWJetsToLNu, TTWJetsToQQ,DYJetsToLL_M_10to50, DYJetsToLL_M_50_a, WWTo2L2Nu, WZTo2L2Q, WZTo3LNu, ZZTo2L2Nu, ZZTo2L2Q, ZZTo4L',
+    'data' : 'MuonEG_2016,SingleElectron_2016,SingleMuon_2016',#,DoubleEG_2017,DoubleMuon_2017',
+      },
+}
+
+
+legendNames = {
+'tt' : 'Prompt t#bar{t}',
+'ttgaus' : 't#bar{t} gaussian',
+'ttnongaus' : 't#bar{t} non-gaussian',
+'stop' : 'stop',
+'Semileptonictt' : 'Semileptonic t#bar{t}',
+'ttDilepNonprompt':'t#bar{t} dilep nonprompt',
+'tWnonprompt': 'tW nonpromtp',
+'Othernonprompt' : 'Other nonprompt',
+}
+
+colors = {
+'tt' : kAzure+2,
+'ttgaus' : kAzure+3,
+'ttnongaus' : kRed-9,
+'stop' : kTeal+2,
+'Semileptonictt' : 1,
+'ttDilepNonprompt': kRed-5,
+'tWnonprompt': kOrange-2,
+'Othernonprompt': kGray,
+'tW' : kOrange+1,
+'ttZ' : kPink+2,
+'data' : 1,
+}
+
+selection = '''
+ if self.outname in ['Semileptonictt', 'ttDilepNonprompt', 'tWnonprompt', 'Othernonprompt']:
+   passNonprompt = (t.TStatus != 1 and t.TStatus != 22)
+   if not passNonprompt: return 
+ elif self.outname != 'data':
+   passNonprompt = (t.TStatus == 1 or  t.TStatus == 22)
+   if not passNonprompt: return 
+'''
+
+
+loopcode = '''
+values = [%i,  %i, t.TDilep_Pt, t.TDeltaPhi, t.TDeltaEta, t.TLep0Pt, t.TLep0Eta, t.TLep1Pt, vmet, t.TLep1Eta, vmll, vmt2, vht]
+prob1 = self.pd1.GetProb(values)
+'''%(ms, ml)
+
+l = looper(path=path[year] if not region=='BS' else pathBS[year], nSlots = 4, treeName = 'MiniTree', options = 'merge', outpath = GetOutPath(year, region)+'/tempfiles/', readOutput=True)
+l.AddHeader('from framework.mva import ModelPredict\n')
+l.AddInit('      self.pd1 = ModelPredict("%s")\n'%model)
+
+processes = processDic[year].keys() #['tt']
+for p in processes: l.AddSample(p,  processDic[year][p])
+
+l.AddSyst(systlist)
+l.AddLoopCode(loopcode)
+
+l.AddExpr('deltaphi', 'TDeltaPhi', 'abs(TDeltaPhi)/3.141592')
+l.AddExpr('deltaeta', 'TDeltaEta', 'abs(TDeltaEta)')
+l.AddExpr('weight1', [], '1')
+l.AddExpr('vpd', '', 'prob1', True) # True because prob1 has to be defined before
+l.AddExpr('vmet', ['TMET'], 'TMET')
+l.AddExpr('vmt2', ['TMT2'], 'TMT2')
+l.AddExpr('vht',  ['THT' ], 'THT')
+l.AddExpr('vmll', ['TMll'], 'TMll')
+l.AddExpr('exprW', ['TWeight'], 'TWeight')
+
+l.AddSelection(selection)
+weight = 'exprW'
+l.AddCut('TPassDilep == 1', 'TPassDilep')
+if region != 'BS':
+  l.AddCut('TMET >= 50', 'TMET')
+  l.AddCut('TMT2 >= 80', 'TMT2')
+l.AddCut('TNJets >= 2', 'TNJets')
+l.AddCut('TNBtags >= 1', 'TNBtags')
+cut = ''
+
+l.AddHisto('vpd',   'dnn_5',  5, 0, 1,   weight = weight, cut = '')
+l.AddHisto('vpd',   'dnn_10', 10, 0, 1,   weight = weight, cut = '')
+l.AddHisto('vpd',   'dnn', 20, 0, 1,   weight = weight, cut = '')
+l.AddHisto('vpd',   'count',  1, 0, 2,   weight = weight, cut = '')
+l.AddHisto('TMll', 'mll', 10, 0, 300, weight = weight, cut = '')
+l.AddHisto('TMET', 'met', 10, 50, 300, weight = weight, cut = '')
+l.AddHisto('TMT2', 'mt2', 8, 80, 160, weight = weight, cut = '')
+l.AddHisto('TMT2', 'mt2_4bins', 4, 80, 160, weight = weight, cut = '')
+l.AddHisto('TDilep_Pt', 'dileppt', 10, 0, 300, weight = weight, cut = '')
+l.AddHisto('TLep0Pt', 'lep0pt', 5, 0, 150, weight = weight, cut = '')
+l.AddHisto('TLep1Pt', 'lep1pt', 10, 0, 300, weight = weight, cut = '')
+l.AddHisto('TLep0Eta', 'lep0eta', 8, -2.4, 2.4, weight = weight, cut = '')
+l.AddHisto('TLep1Eta', 'lep1eta', 8, -2.4, 2.4, weight = weight, cut = '')
+l.AddHisto('deltaphi', 'deltaphi', 8, 0, 1, weight = weight, cut = '')
+l.AddHisto('deltaeta', 'deltaeta', 8, 0, 5, weight = weight, cut = '')
+l.AddHisto('TJet0Pt', 'jet0pt', 10, 0, 450, weight = weight, cut = '')
+l.AddHisto('TJet1Pt', 'jet1pt', 6, 0, 300, weight = weight, cut = '')
+l.AddHisto('TJet0Eta', 'jet0eta', 8, -2.4, 2.4, weight = weight, cut = '')
+l.AddHisto('TJet1Eta', 'jet1eta', 8, -2.4, 2.4, weight = weight, cut = '')
+l.AddHisto('THT', 'ht', 8, 0, 800, weight = weight, cut = '')
+l.AddHisto('TNJets', 'njets', 6, 1.5, 7.5, weight = weight, cut = '')
+l.AddHisto('TNBtags', 'nbtags', 3, 0.5, 3.5, weight = weight, cut = '')
+histos  = ['mt2',  'met',  'mll',  'dnn',  'dileppt',  'deltaphi',  'deltaeta',  'ht',  'lep0pt',  'lep1pt',  'lep0eta',  'lep1eta', 'njets', 'nbtags', 'jet0pt', 'jet1pt', 'jet0eta', 'jet1eta'] 
+
+out = l.Run()
+
+if sendJobs: exit()
+
+# Create HistoManager with the out dictionary from the looper
+processes.pop(processes.index('data'))
+hm = HistoManager(processes, path = path[year] if region == 'SR' else pathBS[year], processDic = processDic[year], lumi = GetLumi(year)*1000,indic = out)
+
+# Function to save the histograms into combine rootfiles
+def save(name):
+  hm.SetHisto(name)
+  hm.Save(outname = outpath+'/'+name, htag = '')
+
+# Save the histograms
+for v in histos: save(v)
